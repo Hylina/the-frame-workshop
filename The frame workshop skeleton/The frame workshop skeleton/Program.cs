7cfb0fb611e7ee361a04ddc1588068e75c953bc4
Hylina <leane.duchet@epita.fr>
2018-02-07 13:28:04 +0100

    Add files via upload

---
+++ The frame workshop skeleton/The frame workshop skeleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Theframeworkshopskeleton
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Point point = new Point("center", 0, 0, 0);
+            Console.WriteLine(point.Name);
+        }
+    }
+}
